45b84d50d2c94e5ef9489e50bd986b30462ab327
namirkhan265-star <namirkhan265@gmail.com>
2025-12-25 16:44:52 +0530

    Bump version to 1.0.2 for edit modal functionality

--- jewellery-price-calculator.php
+++ jewellery-price-calculator.php
@@ -3,7 +3,7 @@
  * Plugin Name: Jewellery Price Calculator
  * Plugin URI: https://github.com/namirkhan265-star/jewellery-price-calculator
  * Description: Automatic jewellery price calculation based on metal rates (Gold, Silver, Diamond, Platinum) with support for making charges, wastage, GST, and discounts
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Brand Witty
  * Author URI: https://bhindi.io
  * Text Domain: jewellery-price-calc
@@ -22,7 +22,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('JPC_VERSION', '1.0.1');
+define('JPC_VERSION', '1.0.2');
 define('JPC_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('JPC_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('JPC_PLUGIN_BASENAME', plugin_basename(__FILE__));
